879bbb22567b4777e237ee972851ab59eedb6caa
mrrobot461 <leulyohannes00@gmail.com>
2026-05-10 15:19:35 +0300

    added auth to main

--- src/main.tsx
+++ src/main.tsx
@@ -1,14 +1,13 @@
-// src/main.tsx
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './App.tsx';
-import { AuthProvider } from './contexts/AuthContext.tsx'; // ADD THIS
+import { AuthProvider } from './contexts/AuthContext.tsx';
 import './index.css';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <AuthProvider> {/* WRAP APP WITH THIS */}
-      <App />
+    <AuthProvider>
+      <App />  {/* App already has BrowserRouter inside */}
     </AuthProvider>
   </StrictMode>,
 );
